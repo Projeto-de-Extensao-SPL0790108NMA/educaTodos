071e1b6171c84e4b61d1c6574346fd2d81eaf453
Eligado <eligadowb@gmail.com>
2025-10-28 20:46:40 -0400

    FIX: Conflicts and wrong customization

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,32 +3,28 @@
 import "./globals.css";
 import {Poppins} from "next/font/google";
 import { theme } from "./theme";
-import ThemeRegistry from "./ThemeRegistry";
 import MainLayout from "./components/MainLayout";
 import { ThemeProvider } from "@emotion/react";
+import { usePathname } from "next/navigation";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
 });
 
-const metadata = {
-  title: "Painel do Aluno",
-  description: "Template Base",
-};
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}) 
+}) {
+  const pathname = usePathname();
+  const isAuthPage = pathname?.includes("/login");
 
-{
   return (
     <html lang="pt-BR" className={poppins.className}>
       <body>
           <ThemeProvider theme={theme}>
-            <MainLayout>{children}</MainLayout>
+            {isAuthPage ? children : <MainLayout>{children}</MainLayout>}
           </ThemeProvider>
       </body>
     </html>
